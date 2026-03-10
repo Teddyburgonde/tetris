909f9cede592ec7af7f4e7667f18261c72dba35f
Teddyburgonde <bandama.teddy@gmail.com>
2026-03-10 15:25:03 +0100

    I have finished the home page, but the CSS for this page still needs to be done.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react'
+import { useNavigate } from "react-router";
+
 function Home() {
+
+	const [playerName, setPlayerName] = useState('')
+	const [roomName, setRoomName] = useState('')
+	const [error, setError] = useState('')
+	let navigate = useNavigate();
+
+	const handlePlay = () => {
+		if (playerName === '' || roomName === '')
+		{
+			setError('Veuillez remplir tous les champs');
+			return;
+		}
+		navigate(`/${roomName}/${playerName}`);
+	}
 	return (
 		<div>
 			<h1>Red tetris</h1>
-			<input type="text" placeholder="Nom du joueur" />
-			<input type="text" placeholder="Nom de la room" />
-			<button></button>
+			<input
+				type="text"
+				placeholder="Nom du joueur"
+				value={playerName}
+				onChange={(e) => setPlayerName(e.target.value)}
+			/>
+			<input
+				type="text"
+				placeholder="Room"
+				value={roomName}
+				onChange={(e) => setRoomName(e.target.value)}
+			/>
+			<button onClick={handlePlay}>Jouer</button>
+			{error && <p>{error}</p>}
 		</div>
 	)
 }
